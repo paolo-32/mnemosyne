fe3ec520304d2651f8647d1ce5a7591275552ecd
paolo-32 <65409747+paolo-32@users.noreply.github.com>
2026-08-05 02:10:26 +0200

    Create tika_extractor.py

---
+++ src/connectors/local_files/tika_extractor.py
@@ -0,0 +1,41 @@
+import os
+from pathlib import Path
+from typing import Protocol
+
+from tika import parser
+
+from .models import ExtractedContent
+
+
+class Extractor(Protocol):
+    def extract(self, path: Path) -> ExtractedContent:
+        ...
+
+
+class TikaExtractor:
+    def __init__(self, endpoint: str) -> None:
+        """Init TikaExtractor.
+
+        Args:
+            endpoint (str): Tika endpoint
+        """
+        self.endpoint = endpoint
+        os.environ["TIKA_SERVER_ENDPOINT"] = endpoint
+
+    def extract(self, path: Path) -> ExtractedContent:
+        """Extract the content of a file with tika.
+
+        Args:
+            path (Path): path object for the file.
+
+        Returns:
+            ExtractedContent: contents of the file.
+        """
+        parsed = parser.from_file(str(path))
+        content = (parsed.get("content") or "").strip()
+        metadata = parsed.get("metadata") or {}
+
+        return ExtractedContent(
+            raw_content=content,
+            metadata=metadata,
+        )
